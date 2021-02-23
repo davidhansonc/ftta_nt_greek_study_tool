e8f84c9efcc96be813512f766c9a7a66838f8337
David Hanson <davidhanson.c@gmail.com>
2021-02-23 16:22:04 -0700

    include script the query database from command line

--- database_creation/create_verse_tables.py
+++ database_creation/create_verse_tables.py
@@ -4,20 +4,19 @@ import pdb
 from bible_book_regex import nt_regex
 
 
-def verses_to_db(db_connection, path_to_file, regex_dict, bible_version):
+def verses_to_db(db_connection, db_cursor, path_to_file, regex_dict, bible_version):
     with open(path_to_file, 'r') as text:
         # pdb.set_trace()
         for line in text:
             line = line.strip()
-            db_verse_insert(regex_dict, line, bible_version)
+            db_verse_insert(db_cursor, regex_dict, line, bible_version)
 
 
-def db_verse_insert(regex_dict, verse_data, bible_version):
+def db_verse_insert(db_cursor, regex_dict, verse_data, bible_version):
     sql_insert = """INSERT INTO new_testament(book, chapter, verse, {}) 
                     VALUES ({}, {}, {}, {})
                     ON CONFLICT (book, chapter, verse) DO 
                     UPDATE SET {} = {};"""
-    cursor = conn.cursor()
     for book_name, regex in regex_dict.items():
         if re.match(regex, verse_data):
             match = re.search(regex, verse_data)
@@ -38,26 +37,36 @@ def db_verse_insert(regex_dict, verse_data, bible_version):
                 bible_version,
                 verse_for_sql
             )
-            cursor.execute(sql)
+            db_cursor.execute(sql)
 
 
-def create_table(db_connection):
-    cursor = conn.cursor()
-    cursor.execute(open('create_nt_table.sql', 'r').read())
+def create_table(db_connection, db_cursor):
+    db_cursor.execute(open('./create_nt_table.sql', 'r').read())
 
 
 if __name__ == '__main__':
     rcv_txt = 'rcv.txt'
     gk_txt = './nestle1904/nestle1904.txt'
 
-    conn = psycopg2.connect(
-        host='localhost',
-        database='na28_rcv',
-        user='postgres',
-        password=''
-    )
-    conn.set_session(autocommit=True)
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='na28_rcv',
+            user='davidhansonc',
+            password=''
+        )
+        conn.set_session(autocommit=True)
+        cursor = conn.cursor()
 
-    create_table(conn)
-    verses_to_db(conn, rcv_txt, nt_regex, 'recovery_version')
-    verses_to_db(conn, gk_txt, nt_regex, 'nestle1904')
+        create_table(conn, cursor)
+        verses_to_db(conn, cursor, rcv_txt, nt_regex, 'recovery_version')
+        verses_to_db(conn, cursor, gk_txt, nt_regex, 'nestle1904')
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL: ", error)
+    
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
---
+++ db_interact.py
@@ -0,0 +1,30 @@
+import psycopg2
+from sys import argv
+
+if __name__ == '__main__':
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='na28_rcv',
+            user='davidhansonc',
+            password=''
+        )
+        cursor = conn.cursor()
+
+        verse_reference = argv[1:4]
+        bible_version = argv[4:]
+        select_query = f"SELECT {bible_version[0]}, {bible_version[1]} FROM new_testament WHERE book={repr(verse_reference[0])} and chapter={repr(verse_reference[1])} and verse={repr(verse_reference[2])};"
+
+        cursor.execute(select_query)
+        verse_selection = cursor.fetchall()
+        print(verse_selection[0][0])
+        print(verse_selection[0][1])
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL: ", error)
+    
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
